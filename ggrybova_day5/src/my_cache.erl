a0099a082fae734644df165b8b0c6c40a03d4f93
ggrybova <gribovaaa@mail.ru>
2018-03-26 21:10:39 +0300

    with out tests

--- ggrybova_day5/src/my_cache.erl
+++ ggrybova_day5/src/my_cache.erl
@@ -5,11 +5,12 @@ create() ->
 	ets:new(cache, [named_table]),
 	ok.
 
-%create(Key, Value, Limit) ->
-%	ets:insert(my_cache:create(), {Key, Value, now(), Limit}).
+create(Key, Value, Limit) ->
+	ets:insert(my_cache:create(), {Key, Value, os:timestamp(), Limit}),
+	ok.
 
 insert(Key, Value, Limit) ->
-	ets:insert(cache, {Key, Value, element(2, now()), Limit}),
+	ets:insert(cache, {Key, Value, element(2, os:timestamp()), Limit}),
 	ok.
 
 lookup(Key) ->
@@ -31,13 +32,23 @@ table_to_list() ->
 check_time(Key) ->
 	[[Lim]] = ets:match(cache, {Key, '_', '_', '$4'}),
 	[[A]] = ets:match(cache, {Key, '_', '$3', '_'}),
-	B = element(2, now()),
+	B = element(2, os:timestamp()),
 	C = B - A,
 	if
 		C < Lim -> 1;
 		C >= Lim -> 0
 	end.
 
+delete_obsolete() ->
+	delete_obsolete(ets:first(cache)).
 
-%delete() ->
-%	ets:match_delete(cache, {'_', '-', '_'}).
+delete_obsolete(Id) ->
+	Next = ets:next(cache, Id),	
+	case lookup(Id) of
+		[] -> delete(Id);
+		_ -> ok
+	end,
+	case Next of
+		'$end_of_table' -> ok;
+		_ -> delete_obsolete(Next)
+	end.
